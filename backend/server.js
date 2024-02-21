0a1393fea76d933b1e372a901ca8648fe7fb7932
Tejas-gaikwad <tejasgaikwad0504@gmail.com>
2024-02-21 12:29:43 +0530

    intial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+var cors = require('cors')
+
+app.use(cors());
+
+// middlewares
+app.use(express.json({ extended: false }));
+
+// HOME
+app.use("/", require("./routes/home"));
+// route included
+app.use("/payment", require("./routes/payment"));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
